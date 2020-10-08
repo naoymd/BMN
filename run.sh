534348415b64d763db194f2f6ace393f54d7eb18
naoymd <nyamada@aoki-medialab.jp>
2020-10-08 12:48:47 +0900

    2020/10/08

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
 python main.py ./experiments/config.yaml --no_wandb
+python main.py ./experiments/config0.yaml --no_wandb
+python main.py ./experiments/config1.yaml --no_wandb
+python main.py ./experiments/config2.yaml --no_wandb
